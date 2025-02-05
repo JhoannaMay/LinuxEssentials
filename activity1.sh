9d3fe818d0a6b0a4b651427054e6b778f46055c8
Jhoanna May Lacorte <152822639+JhoannaMay@users.noreply.github.com>
2025-02-05 01:44:43 +0000

    LAB ACTIVITY 1

---
+++ activity1.sh
@@ -0,0 +1,13 @@
+echo "Activity 1"
+
+
+echo "Enter your name: "
+read name
+
+echo "Enter your age: "
+read age
+
+echo "Enter your program: "
+read program
+
+echo "Hello $name!, this is your age $age and your program is $program."
